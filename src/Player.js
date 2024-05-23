e74330a80f94c5820d5d5ebd5bf5d15eab031768
Polychroma <support@polychroma.tv>
2024-05-23 09:34:01 -0230

    Update Player.js

--- src/Player.js
+++ src/Player.js
@@ -28,72 +28,72 @@ const defaultPlayerVars = {
     rel: 0,
     muted: 1,
     origin: 'https://beta-peach.vercel.app/'
-};
-
-class Player extends React.Component {
+  };
+  
+  class Player extends React.Component {
     playerRef;
-
+  
     constructor(props) {
-        super(props);
-
-        this.onEnd = this.onEnd.bind(this);
-        this.onError = this.onError.bind(this);
-        this.onReady = this.onReady.bind(this);
-        this.onStateChange = this.onStateChange.bind(this);
-        this.onPlaybackQualityChange = this.onPlaybackQualityChange.bind(this);
-        this.gameLoop = this.gameLoop.bind(this);
-
-        this.playerRef = React.createRef();
-
-        this.state = {
-            playerStatus: undefined
-        }
-
-        this.updateChannelData();
+      super(props);
+  
+      this.onEnd = this.onEnd.bind(this);
+      this.onError = this.onError.bind(this);
+      this.onReady = this.onReady.bind(this);
+      this.onStateChange = this.onStateChange.bind(this);
+      this.onPlaybackQualityChange = this.onPlaybackQualityChange.bind(this);
+      this.gameLoop = this.gameLoop.bind(this);
+  
+      this.playerRef = React.createRef();
+  
+      this.state = {
+        playerStatus: undefined
+      }
+  
+      this.updateChannelData();
     }
-
+  
     updateChannelData() {
-        const channelData = this.props.channelData;
-        if (channelData && channelData.currentVideo) {
-            const videoId = channelData.currentVideo.fields['id'];
-            const videoEnd = channelData.currentVideo.fields.duration * 60;
-
-            const guideCreatedAt = new Date(this.props.guideCreatedAt);
-            const videoStartOffset = Math.ceil((new Date() - guideCreatedAt) / 1000);
-            const videoStart = channelData.videoStart + videoStartOffset;
-
-            // Delay update of Player data to give time to animation transition
-            setTimeout(() => {
-                this.setState({
-                    videoId,
-                    playerOpts: {
-                        playerVars: {
-                            ...defaultPlayerVars,
-                            start: videoStart,
-                            end: videoEnd
-                        },
-                    }
-                })
-            }, VIDEO_TRANSITION_MS);
-        }
+      const channelData = this.props.channelData;
+      if (channelData && channelData.currentVideo) {
+        const videoId = channelData.currentVideo.fields['id'];
+        const videoEnd = channelData.currentVideo.fields.duration * 60;
+  
+        const guideCreatedAt = new Date(this.props.guideCreatedAt);
+        const videoStartOffset = Math.ceil((new Date() - guideCreatedAt) / 1000);
+        const videoStart = channelData.videoStart + videoStartOffset;
+  
+        // Delay update of Player data to give time to animation transition
+        setTimeout(() => {
+          this.setState({
+            videoId,
+            playerOpts: {
+              playerVars: {
+                ...defaultPlayerVars,
+                start: videoStart,
+                end: videoEnd
+              },
+            }
+          })
+        }, VIDEO_TRANSITION_MS);
+      }
     }
-
+  
     componentDidUpdate(prevProps, prevState) {
         if (prevProps.isMuted !== this.props.isMuted ||
             prevProps.volume !== this.props.volume) {
             this.updateVolume();
         }
-
+        
         if (prevProps.channelData !== this.props.channelData) {
             this.setState({
                 playerStatus: undefined,
                 videoId: undefined
             })
-
+        
             this.updateChannelData();
         }
     }
-
+        
     onReady(e) {
         this.updateVolume();
 
@@ -106,15 +106,20 @@ class Player extends React.Component {
 
         this.consecutiveUnstarted = 0;
 
-        if (this.props.channelData.currentVideo.fields.playerType === 'HTML5') {
-            e.target.play();
+        // Sync HTML5 video to the correct start time
+        if (this.props.channelData.currentVideo.fields.playerType !== 'YouTube') {
+            const videoElement = this.playerRef.current;
+            const videoStart = this.state.playerOpts.playerVars.start;
+            if (videoElement && videoStart) {
+                videoElement.currentTime = videoStart;
+            }
         }
     }
-
+    
     gameLoop() {
         if (this.playerRef.current) {
             let opts = {};
-
+    
             // If video play failed we need to manually send to Analytics its data
             if (this.state.playerStatus === 'error') {
                 const channelData = this.props.channelData;
@@ -125,18 +130,18 @@ class Player extends React.Component {
                     }
                 }
             }
-
+    
             Analytics.event('player_status_' + this.state.playerStatus, opts);
-
+    
             if (this.state.playerStatus === 'unstarted'
                 || this.state.playerStatus === undefined) {
                 this.consecutiveUnstarted += 1;
                 console.debug('consecutiveUnstarted', this.consecutiveUnstarted);
-
+    
                 if (this.consecutiveUnstarted > UNSTARTED_TIMEOUT_TO_MUTE) {
                     console.debug('force set muted');
                     this.props.setMuted();
-
+    
                     if (this.consecutiveUnstarted > UNSTARTED_TIMEOUT_GIVEUP) {
                         this.setState({playerStatus: 'error'});
                     }
@@ -144,7 +149,7 @@ class Player extends React.Component {
             } else {
                 this.consecutiveUnstarted = 0;
             }
-
+    
             // Just to make sure (also turns it up after computer sleeping)
             if (this.props.channelData.currentVideo.fields.playerType === 'YouTube') {
                 this.playerRef.current.internalPlayer.playVideo();
@@ -153,12 +158,12 @@ class Player extends React.Component {
             }
         }
     }
-
+    
     updateVolume() {
         if (this.playerRef.current) {
             if (this.props.channelData.currentVideo.fields.playerType === 'YouTube') {
-                this.playerRef.current.internalPlayer.setVolume(this.props.volume * 100);
-
+                this.playerRef.current.internalPlayer.setVolume(this.props.volume*100);
+    
                 if (this.props.isMuted) {
                     this.playerRef.current.internalPlayer.mute();
                 } else {
@@ -170,29 +175,29 @@ class Player extends React.Component {
             }
         }
     }
-
+    
     onEnd() {
         console.debug('onEnd');
         this.props.onVideoEnd();
     }
-
+    
     onError(e) {
         const msg = {
-            2: 'The request contains an invalid parameter value. For example, this error occurs if you specify a video ID that does not have 11 characters, or if the video ID contains invalid characters, such as exclamation points or asterisks.'
-            , 5: 'The requested content cannot be played in an HTML5 player or another error related to the HTML5 player has occurred.'
-            , 100: 'The video requested was not found. This error occurs when a video has been removed (for any reason) or has been marked as private.'
-            , 101: 'The owner of the requested video does not allow it to be played in embedded players.'
-            , 150: 'The owner of the requested video does not allow it to be played in embedded players.'
+             2: 'The request contains an invalid parameter value. For example, this error occurs if you specify a video ID that does not have 11 characters, or if the video ID contains invalid characters, such as exclamation points or asterisks.'
+            ,5: 'The requested content cannot be played in an HTML5 player or another error related to the HTML5 player has occurred.'
+            ,100: 'The video requested was not found. This error occurs when a video has been removed (for any reason) or has been marked as private.'
+            ,101: 'The owner of the requested video does not allow it to be played in embedded players.'
+            ,150: 'The owner of the requested video does not allow it to be played in embedded players.'
         };
         console.warn('YouTube Player error', e.data, msg[e.data]);
-
+    
         this.setState({
             playerStatus: 'error'
         });
-
+        
         this.props.skipVideo();
     }
-
+    
     onStateChange(e) {
         // Source: https://developers.google.com/youtube/iframe_api_reference#onStateChange
         const msg = [
@@ -209,21 +214,21 @@ class Player extends React.Component {
         this.setState({
             playerStatus: statusStr
         });
-
+    
         // Force captions off
         const player = e.target;
         player.unloadModule('captions');
         player.unloadModule('cc');
     }
-
+    
     onPlaybackQualityChange(e) {
         console.debug('onPlaybackQualityChange', e.data);
     }
-
+    
     render() {
         const { isUIVisible, t } = this.props;
         const { playerOpts, videoId, playerStatus } = this.state;
-
+    
         return (
             <div className="overflow-hidden">
                 <div
@@ -231,71 +236,71 @@ class Player extends React.Component {
                         `video-background overflow-hidden transition-opacity duration-1000
                         ${isUIVisible ? 'opacity-75 hover:opacity-100' : 'opacity-100'}`}
                     style={{
-                        transition: `transform ${CHROMA_EASING_TIMING} ${CHROMA_EASING_BEZIER}, opacity 300ms ease-out`,
-                        transform: isUIVisible ?
-                            `translate(${MAIN_BAR_WIDTH}px, -${BOTTOM_BAR_HEIGHT}px)` :
-                            `translate(0, 0)`,
-                        cursor: isUIVisible ? 'zoom-in' : '',
+                    transition: `transform ${CHROMA_EASING_TIMING} ${CHROMA_EASING_BEZIER}, opacity 300ms ease-out`,
+                    transform: isUIVisible ? 
+                        `translate(${MAIN_BAR_WIDTH}px, -${BOTTOM_BAR_HEIGHT}px)`:
+                        `translate(0, 0)`,
+                    cursor: isUIVisible ? 'zoom-in': '',
+                }}
+                onClick={isUIVisible ? this.props.onPlayerClick : undefined}>
+                
+                <div
+                    className="absolute w-full h-full bg-gray-100 flex items-center justify-center"
+                    style={{
+                        paddingRight: isUIVisible && MAIN_BAR_WIDTH,
+                        paddingTop: isUIVisible && BOTTOM_BAR_HEIGHT
                     }}
-                    onClick={isUIVisible ? this.props.onPlayerClick : undefined}>
-
-                    <div
-                        className="absolute w-full h-full bg-gray-100 flex items-center justify-center"
-                        style={{
-                            paddingRight: isUIVisible && MAIN_BAR_WIDTH,
-                            paddingTop: isUIVisible && BOTTOM_BAR_HEIGHT
-                        }}
-                    >
-                        {
-                            playerStatus === 'error' ?
-                                <div className="max-w-xs text-center text-gray-500">
-                                    {t('video-error')}
-                                </div>
-                                : playerStatus !== 'playing' &&
-                                <div className="h-10 w-10 text-gray-400">
-                                    <Spinner />
-                                </div>
-                        }
-                    </div>
-
+                >
                     {
-                        videoId &&
-                        <div className={`video-foreground
+                        playerStatus === 'error' ?
+                            <div className="max-w-xs text-center text-gray-500">
+                                {t('video-error')}
+                            </div>
+                        : playerStatus !== 'playing' &&
+                            <div className="h-10 w-10 text-gray-400">
+                                <Spinner />
+                            </div>
+                    }
+                </div>
+                
+                {
+                    videoId &&
+                    <div className={`video-foreground
                         transition-opacity ease-in-out duration-${VIDEO_TRANSITION_MS}
                         ${playerStatus === 'playing' ? 'opacity-100' : 'opacity-0'}
                     `}>
-                            {
-                                this.props.channelData.currentVideo.fields.playerType === 'YouTube' ?
-                                    <YouTube
-                                        videoId={videoId}
-                                        opts={playerOpts}
-                                        onEnd={this.onEnd}
-                                        onError={this.onError}
-                                        onReady={this.onReady}
-                                        onStateChange={this.onStateChange}
-                                        onPlaybackQualityChange={this.onPlaybackQualityChange}
-                                        ref={this.playerRef}
-                                    /> :
-                                    <video
-                                        className="video-foreground"
-                                        src={videoId}
-                                        controls
-                                        autoPlay
-                                        muted={this.props.isMuted}
-                                        volume={this.props.volume}
-                                        onEnded={this.onEnd}
-                                        onError={this.onError}
-                                        ref={this.playerRef}
-                                        onCanPlay={this.onReady}
-                                        onPlaying={() => this.setState({ playerStatus: 'playing' })}
-                                    />
-                            }
-                        </div>
-                    }
-                </div>
+                        {
+                            this.props.channelData.currentVideo.fields.playerType === 'YouTube' ?
+                            <YouTube
+                                videoId={videoId}
+                                opts={playerOpts}
+                                onEnd={this.onEnd}
+                                onError={this.onError}
+                                onReady={this.onReady}
+                                onStateChange={this.onStateChange}
+                                onPlaybackQualityChange={this.onPlaybackQualityChange}
+                                ref={this.playerRef}
+                            /> :
+                            <video
+                                className="video-foreground"
+                                src={videoId}
+                                controls
+                                autoPlay
+                                muted={this.props.isMuted}
+                                volume={this.props.volume}
+                                onEnded={this.onEnd}
+                                onError={this.onError}
+                                ref={this.playerRef}
+                                onCanPlay={this.onReady}
+                                onPlay={() => this.setState({ playerStatus: 'playing' })}
+                            />
+                        }
+                    </div>
+                }
             </div>
-        );
+        </div>
+    );
     }
 }
-
+    
 export default withTranslation()(Player);
