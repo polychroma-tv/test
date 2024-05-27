34e1f293af666b9322f63edeb678c3021a113f04
Polychroma <support@polychroma.tv>
2024-05-27 15:26:49 -0230

    Update BottomBar.js

--- src/BottomBar.js
+++ src/BottomBar.js
@@ -194,167 +194,171 @@ class BottomBar extends React.Component {
   }
 
   render() {
-  const { t } = this.props;
+    const { t } = this.props; 
 
-  let currentVideo, nextVideo;
-  let time1Str, time2Str;
-  let channelTitle, channelUrl;
-  let currentVideoTitle, currentVideoUrl;
-  let nextVideoTitle;
-  let shouldShowNextVideo;
+    let currentVideo, nextVideo;
+    let time1Str, time2Str;
+    let channelTitle, channelUrl;
+    let currentVideoTitle, currentVideoUrl;
+    let nextVideoTitle;
+    let shouldShowNextVideo;
 
-  if (this.state.channelData) {
-    ({ time1Str, time2Str } = this.getFormatedTimes());
-    ({ currentVideo, nextVideo } = this.state.channelData);
+    if (this.state.channelData) {
+      ({ time1Str, time2Str } = this.getFormatedTimes()); 
+      ({ currentVideo, nextVideo } = this.state.channelData);
+      
+      channelTitle =
+        currentVideo.fields['channelTitle'] &&
+        currentVideo.fields['channelTitle'][0];
+      channelUrl =
+        currentVideo.fields['channelUrl'] &&
+        currentVideo.fields['channelUrl'][0];
 
-    channelTitle =
-      currentVideo.fields['channelTitle'] &&
-      currentVideo.fields['channelTitle'][0];
-    channelUrl =
-      currentVideo.fields['channelUrl'] &&
-      currentVideo.fields['channelUrl'][0];
+      // Use the same method for both html5 and YouTube
+      currentVideoUrl = `title/${currentVideo.fields['id']}`;
+      
+      currentVideoTitle = stripEmojis(currentVideo.fields['title']);
+      nextVideoTitle = stripEmojis(nextVideo.fields['title']);
 
-    // Use the same method for both html5 and YouTube
-    currentVideoUrl = `title/${currentVideo.fields['id']}`;
+      const remainingTimeSec = (new Date(this.state.channelData.time2) - new Date()) / 1000;
+      shouldShowNextVideo = 
+        nextVideoTitle &&
+        remainingTimeSec < 10 * 60
+    }
 
-    currentVideoTitle = stripEmojis(currentVideo.fields['title']);
-    nextVideoTitle = stripEmojis(nextVideo.fields['title']);
+    // let latlong, latlongLabel;
+    // latlong = currentVideo.fields['latlong'];
+    // latlongLabel = latlong && latlong.split(',').map(i => i+'°').join(' ');
 
-    const remainingTimeSec = (new Date(this.state.channelData.time2) - new Date()) / 1000;
-    shouldShowNextVideo =
-      nextVideoTitle &&
-      remainingTimeSec < 10 * 60;
-  }
-
-  return (
-    <div className={`
-      bottom-bar bg-white z-1 fixed left-0 bottom-0 w-full
-      transform transition-all
-      ${this.props.isUIVisible || this.state.open ? '-translate-y-0 opacity-100' : 'translate-y-full opacity-0'}
-    `} style={{
-      transitionTimingFunction: CHROMA_EASING_BEZIER,
-      transitionDuration: CHROMA_EASING_TIMING
-    }}>
-      <div
-        className="pt-2 flex justify-between text-teal-800 font-body"
-        style={{
-          paddingLeft: MAIN_BAR_WIDTH,
-          height: BOTTOM_BAR_HEIGHT + 'px'
-        }}
-      >
-        <div className={` 
-          flex w-10/12 transform transition-all ease-out
-          ${(this.props.isUIVisible && !this.state.loading) || (this.state.open) ? '-translate-y-0 opacity-100' : '-translate-y-2 opacity-0'}`}
-          style={{ transitionDuration: LABELS_TRANSITION_MS + 'ms' }}
+    return (
+      <div className={`
+        bottom-bar bg-white z-1 fixed left-0 bottom-0 w-full
+        transform transition-all
+        ${this.props.isUIVisible || this.state.open ? '-translate-y-0 opacity-100' : 'translate-y-full opacity-0'}
+      `} style={{
+        transitionTimingFunction: CHROMA_EASING_BEZIER,
+        transitionDuration: CHROMA_EASING_TIMING
+      }}>
+        <div
+          className="pt-2 flex justify-between text-teal-800 font-body"
+          style={{
+            paddingLeft: MAIN_BAR_WIDTH,
+            height: BOTTOM_BAR_HEIGHT + 'px'
+          }}
         >
-          <div className={`${isMobile ? 'w-10/12' : 'w-6/12'} pr-8 flex flex-col`}>
-            <div className="mt-2 text-xs font-extrabold whitespace-no-wrap mb-1">
-              {time1Str}
-            </div>
-
-            <div className="flex justify-between">
-              <div className="flex truncate">
-                <div className="truncate">
-                  <div className={`truncate ${isMobile ? '' : 'text-xl'}`}>
-                    <button className="hover:underline" onClick={() => {
-                      this.props.history.push(`/${currentVideoUrl}`);
-                      // Trigger onDemandVideo function here
-                      this.props.onDemandVideo();
-                    }}>
-                      {currentVideoTitle}
-                    </button>
-                  </div>
-
-                  <div>
-                    <a className=" hover:underline" href={channelUrl}>
-                      {channelTitle}
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="mt-2 text-xs additional-info">
-              {this.state.additionalInfo.contentRating && <div className="content-rating">{this.state.additionalInfo.contentRating}</div>}
-              {this.state.additionalInfo.genre && <div>{this.state.additionalInfo.genre}</div>}
-              {this.state.additionalInfo.year && <div>({this.state.additionalInfo.year})</div>}
-            </div>
-            <div className="mt-2 text-xs description">
-              {this.state.currentVideoDescription}
-            </div>
-          </div>
-
-          {
-            shouldShowNextVideo &&
-            <BrowserView viewClassName="w-5/12 pr-8 flex flex-col text-gray-400">
-              {/* <div className="mb-1 mt-2 h-2px w-full bg-gray-300"/> */}
-              <div className="mt-2 text-xs font-extrabold whitespace-no-wrap mb-1">
-                {t('later')}
-              </div>
-
-              <div className="flex">
-                <div className="truncate">
-                  <div className="truncate text-xl">
-                    {nextVideoTitle}
-                  </div>
-                </div>
-              </div>
-            </BrowserView>
-          }
-        </div>
-
-        <div className="w-2/12 mt-4 pr-2 flex justify-end items-start">
-          <div
-            className="relative"
-            onMouseEnter={() => this.setState({ showVolumeSlider: true })}
-            onMouseLeave={() => this.setState({ showVolumeSlider: false })}
+          <div className={` 
+            flex w-10/12 transform transition-all ease-out
+            ${(this.props.isUIVisible && !this.state.loading) || (this.state.open) ? '-translate-y-0 opacity-100' : '-translate-y-2 opacity-0'}`}
+            style={{ transitionDuration: LABELS_TRANSITION_MS + 'ms' }}
           >
-            {
-              !isMobile &&
-              this.state.showVolumeSlider &&
-              <div
-                className={`
-                  absolute flex bg-white p-5 rounded-lg
-                  transition-all ease-in-out duration-300
-                  ${this.state.showVolumeSlider ? 'opacity-100' : 'opacity-0'}
-                `}
-                style={{
-                  transform: `rotate(-90deg)`,
-                  top: -112,
-                  right: -56
-                }}
-              >
-                <input
-                  className={this.props.isMuted ? undefined : `cursor-pointer`}
-                  type="range" min={0} max={1} step={0.02}
-                  value={this.props.volume}
-                  disabled={this.props.isMuted}
-                  onChange={e => {
-                    this.props.onChangeVolume(e.target.value);
-                  }}
-                />
+            <div className={`${isMobile ? 'w-10/12' : 'w-6/12'} pr-8 flex flex-col`}>
+              <div className="mt-2 text-xs font-extrabold whitespace-no-wrap mb-1">
+                {time1Str}
               </div>
-            }
 
-            <button className={`
-              p-5 hover:bg-gray-200 transition-colors duration-300 rounded-lg
-              ${this.props.isMuted ? 'text-red-700' : ''} `}
-              onClick={this.props.onToggleMute}>
-              <IconVolume isMuted={this.props.isMuted} />
-            </button>
+              <div className="flex justify-between">
+                <div className="flex truncate">
+                  <div className="truncate">
+                    <div className={`truncate ${isMobile ? '' : 'text-xl'}`}>
+                      {/* Integration of onClick handler for currentVideoTitle */}
+                      <button className="hover:underline" onClick={() => {
+                        this.props.history.push(`/${currentVideoUrl}`);
+                        this.props.onDemandVideo(); // Trigger onDemandVideo function
+                      }}>
+                        {currentVideoTitle}
+                      </button>
+                    </div>
+
+                    <div>
+                      <a className=" hover:underline" href={channelUrl}>
+                        {channelTitle}
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-2 text-xs additional-info">
+                {this.state.additionalInfo.contentRating && <div className="content-rating">{this.state.additionalInfo.contentRating}</div>}
+                {this.state.additionalInfo.genre && <div>{this.state.additionalInfo.genre}</div>}
+                {this.state.additionalInfo.year && <div>({this.state.additionalInfo.year})</div>}
+              </div>
+              <div className="mt-2 text-xs description">
+                {this.state.currentVideoDescription}
+              </div>
+            </div>
+
+                {
+                  shouldShowNextVideo &&
+                  <BrowserView viewClassName="w-5/12 pr-8 flex flex-col text-gray-400">
+                      {/* <div className="mb-1 mt-2 h-2px w-full bg-gray-300"/> */}
+                      <div className="mt-2 text-xs font-extrabold whitespace-no-wrap mb-1">
+                        { t('later') }
+                      </div>
+
+                      <div className="flex">
+                        <div className="truncate">
+                          <div className="truncate text-xl">
+                            { nextVideoTitle }
+                          </div>
+                        </div>
+                      </div>
+                  </BrowserView>
+                }
+              </div>
+              
+              <div className="w-2/12 mt-4 pr-2 flex justify-end items-start">
+                <div
+                  className="relative"
+                  onMouseEnter={() => this.setState({showVolumeSlider: true})} 
+                  onMouseLeave={() => this.setState({showVolumeSlider: false})}
+                >
+                  {
+                    !isMobile &&
+                    this.state.showVolumeSlider &&
+                    <div
+                      className={`
+                        absolute flex bg-white p-5 rounded-lg
+                        transition-all ease-in-out duration-300
+                        ${this.state.showVolumeSlider ? 'opacity-100' : 'opacity-0'}
+                      `}
+                      style={{
+                        transform: `rotate(-90deg)`,
+                        top: -112,
+                        right: -56
+                        }}>
+                      <input
+                        className={this.props.isMuted ? undefined : `cursor-pointer`}
+                        type="range" min={0} max={1} step={0.02}
+                        value={this.props.volume}
+                        disabled={this.props.isMuted}
+                        onChange={e => {
+                          this.props.onChangeVolume(e.target.value);
+                        }}
+                      />
+                    </div>
+                  }
+
+                  <button className={`
+                    p-5 hover:bg-gray-200 transition-colors duration-300 rounded-lg
+                    ${this.props.isMuted ? 'text-red-700' : ''} `}
+                    onClick={this.props.onToggleMute}>
+                      <IconVolume isMuted={this.props.isMuted}/>
+                  </button>
+                </div>
+                
+                {
+                  Screenfull.isEnabled &&
+                  <button
+                    className="p-5 hover:bg-gray-200 transition-colors duration-300 rounded-lg"
+                    onClick={this.props.onToggleFullscreen}>
+                      <IconFullScreen isFullScreen={Screenfull.isFullscreen}/>
+                  </button>
+                }
+              </div>
           </div>
-
-          {
-            Screenfull.isEnabled &&
-            <button
-              className="p-5 hover:bg-gray-200 transition-colors duration-300 rounded-lg"
-              onClick={this.props.onToggleFullscreen}>
-              <IconFullScreen isFullScreen={Screenfull.isFullscreen} />
-            </button>
-          }
         </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default withRouter(withTranslation()(BottomBar));
